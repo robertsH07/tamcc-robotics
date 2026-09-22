158dc1376be3fe1299fc8d2e91bd60f258546093
robertsH07 <784chocolate@gmail.com>
2026-09-22 03:27:45 -0400

    Refactor createMeeting function for clarity

--- js-meeting.js
+++ js-meeting.js
@@ -11,9 +11,7 @@ function createMeeting() {
     localStorage.setItem("meetingDate", date);
     localStorage.setItem("meetingID", meetingID);
 
-    const attendanceURL =
-        "https://robertsh07.github.io/TAMCC-Robotics/attendance-page.html?meeting=" +
-        meetingID;
+    const attendanceURL = "https://robertsh07.github.io/TAMCC-Robotics/attendance-page.html?meeting=" + meetingID;
 
     const qrContainer = document.getElementById("qrcode");
 
@@ -25,30 +23,3 @@ function createMeeting() {
     console.log("Meeting Date:", date);
     console.log("Attendance URL:", attendanceURL);
 }
-
-function createAttendanceQR() {
-    const meetingID = localStorage.getItem("meetingID");
-
-    if (!meetingID) {
-        alert("No meeting has been created.");
-        return;
-    }
-
-    const attendanceURL =
-        "https://robertsh07.github.io/TAMCC-Robotics/attendance-page.html?meeting=" +
-        meetingID;
-
-    const qrContainer = document.getElementById("qrcode");
-
-    qrContainer.innerHTML = "";
-
-    new QRCode(qrContainer, attendanceURL);
-
-    console.log("Meeting ID:", meetingID);
-    console.log("Attendance URL:", attendanceURL);
-}
-
-const parameters = new URLSearchParams(window.location.search);
-const urlMeetingID = parameters.get("meeting");
-
-console.log("Meeting ID:", urlMeetingID);
